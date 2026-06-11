fecfb386ddd6045bb52371f020b0b3a520fdc144
kavidu Purnamal <225520780+K4V1DU@users.noreply.github.com>
2026-06-11 17:34:28 +0530

    rrtrte

--- frontend/src/Components/AddAccommodation/AddAccommodation.js
+++ frontend/src/Components/AddAccommodation/AddAccommodation.js
@@ -454,7 +454,7 @@ const AddAccommodation = () => {
         <div className="hn-nav__logo-wrap">
           <a href="/Listings" className="hn-nav__logo">
             <img
-              src={showForm ? "/Images/66.png" : "/Images/logo6.png"}
+              src={showForm ? "/Images/66.png" : "/Images/66.png"}
               alt="Boodima Logo"
               style={{ height: "32px", width: "auto", display: "block" }}
             />
